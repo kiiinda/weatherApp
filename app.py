955920b9d2d2bc6aa1a63c6273327169cab20638
oknyawira@gmail.com <oknyawira@gmail.com>
2023-01-24 05:48:56 +0300

    Fetch data from API and display the data

--- app.py
+++ app.py
@@ -10,9 +10,18 @@ def index():
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=882360f00976cbb441f3ca5038c5c5d7"
     city = 'Vegas'
 
-    r = requests.get(url.format(city)).json()  #request to api
-    print(r)
-    return render_template("weather.html")
+    r = requests.get(url.format(city)).json()#request to api
+
+
+    weather = {
+        'city' : 'Nairobi',
+        'temparature': r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon']
+    }
+
+    print(weather)
+    return render_template("weather.html", weather=weather)#pass weather to template
 
 if __name__ == '__main__':
     app.run(debug=True)#start server
